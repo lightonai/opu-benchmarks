fd5a3a4c4a3c9cbe0aaae92820aaa3f2b1444405
LucaT1995 <lu.tom1995@gmail.com>
2020-04-22 13:04:01 +0200

    Add Newma code on graphs without random projection

---
+++ scripts/graphs/newma_graph.py
@@ -0,0 +1,71 @@
+import os
+import pathlib
+import argparse
+from argparse import RawTextHelpFormatter
+
+from random import shuffle
+
+import networkx as nx
+import numpy as np
+import time
+
+from utils.graphs.newma import Graph, NEWMA
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Change point detection on graphs with NEWMA.",
+                                     formatter_class=RawTextHelpFormatter)
+
+    # Graph arguments
+    parser.add_argument("-n", '--n_nodes', help="Number of nodes of the graph.", type=int, default=1000)
+    parser.add_argument("-p", '--p_edges', help="Probability of drawing an edge between two nodes.", type=float, default=0.03)
+    parser.add_argument("-cs", '--clique_size', help="Size of the clique.", type=int, default=20)
+    parser.add_argument("-nr", '--noise_ratio', help="percentage of the total number of edges to add/remove as noise.",
+                        type=int, default=10)
+    parser.add_argument("-t", '--t_end', help="Duration of the simulation.", type=int, default=100)
+    parser.add_argument("-tc", '--t_clique', help="Time of creation of the clique.", type=int, default=60)
+
+    #NEWMA arguments
+    parser.add_argument("-tw", '--time_window', help="Time window for the detection of changes.", type=int, default=20)
+    parser.add_argument("-lr", '--l_ratio', help="Ratio between the forgetting factors.", type=float, default=8.5)
+    parser.add_argument("-eta", '--eta', help="Forgetting factor for threshold.", type=float, default=0.99)
+    parser.add_argument("-r_tau", '--rescale_tau', help="rescaling factor for threshold.", type=float, default=1.07)
+    parser.add_argument("-nc", '--n_components', help="Number of random projections.", type=int, default=1000)
+
+    parser.add_argument("-s", "--save_path", help='Path to the save folder. If None, results will not be saved. Default=None.',
+                        type=str, default=None)
+    args = parser.parse_args()
+    return args
+
+def main(args):
+
+    if args.save_path is not None:
+        folder_name = os.path.join(args.save_path, "graph_nodes_{}_clique_{}".format(args.n_nodes, args.clique_size))
+        pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+
+    graph = Graph(n_nodes=args.n_nodes, p_edges=args.p_edges, clique_size=args.clique_size, noise_ratio=args.noise_ratio)
+    graph.__info__()
+
+    newma = NEWMA(args.n_nodes, args.n_components, time_window=args.time_window, l_ratio=args.l_ratio, eta=args.eta,
+                  rescale_tau=args.rescale_tau, save_path=folder_name)
+
+    for t in range(1, args.t_end):
+        new_edges = graph.evolve()
+        #print('t = {0:4d}\t# edges = {1:6d}'.format(t, graph.G.number_of_edges()))
+
+        if t == args.t_clique:
+            graph.create_clique()
+
+        Adj_matrix = nx.to_numpy_array(graph.G)
+
+        newma.detect(Adj_matrix, t=t)
+        eigenvector = newma.compute_eigenvector(Adj_matrix)
+
+        print(newma.log[-1])
+        #print('t = {0:4d}\t# edges = {1:6d}\tnorm = {2:4.1f}\tprev_average = {3:4.1f}\ttau = {4:4.1f}'
+        #      .format(t, graph.G.number_of_edges(), norm, S_bar, threshold))
+    return
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
---
+++ utils/graphs/newma.py
@@ -0,0 +1,210 @@
+import os
+
+from random import shuffle
+from itertools import combinations
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pandas as pd
+
+class Graph:
+
+    def __init__(self, n_nodes, clique_size, p_edges=0.003, noise_ratio=10, save_path=None):
+        self.n_nodes = n_nodes
+        self.p_edges = p_edges
+        self.clique_size = clique_size
+        self.noise_ratio = noise_ratio
+        self.save_path = save_path
+
+        self.G = nx.gnp_random_graph(n=self.n_nodes, p=self.p_edges)
+
+        self.n_noise_edges = np.ceil(self.noise_ratio / 100 * len(self.G.edges))
+        self.nodes_clique, self.edges_clique, self.pos_dict = self.initialize_clique()
+
+        if self.save_path is not None:
+            self.fig = plt.figure(figsize=(9.5, 7.5))
+
+
+    def initialize_clique(self):
+        nodes = list(self.G.nodes)
+
+        shuffle(nodes)
+        nodes_clique = nodes[0:self.clique_size]
+
+        pos_list = [(-np.random.rand() * 5, -5 - np.random.rand() * 10) for i in range(self.n_nodes)]
+        nodes_color = ['r' for i in range(self.n_nodes)]
+
+        for i in range(len(nodes_clique)):
+            pos_list[nodes_clique[i]] = (2 + np.random.rand() * 5, -5 - np.random.rand() * 10)
+            nodes_color[nodes_clique[i]] = 'b'
+
+        pos_dict = dict(zip(self.G.nodes(), pos_list))
+
+        edges_clique = []
+
+        for (u, v) in combinations(nodes_clique, r=2):
+            edges_clique.append((u, v))
+
+        return nodes_clique, edges_clique, pos_dict
+
+    def evolve(self):
+        """
+        Evolves in time the graph G by adding and removing edges.
+
+        Returns
+        -------
+        no_edges: list of tuples,
+            edges that were added. Useful for plots
+        """
+
+        n_add = np.random.randint(0, self.n_noise_edges + 1)
+        n_rem = np.random.randint(0, self.n_noise_edges + 1)
+
+        edges = list(self.G.edges)
+        shuffle(edges)
+        self.G.remove_edges_from(edges[0:n_rem])
+
+        no_edges = list(nx.non_edges(self.G))
+        shuffle(no_edges)
+        self.G.add_edges_from(no_edges[0:n_add])
+
+        return no_edges[0:n_add]
+
+    def create_clique(self):
+        self.G.add_edges_from(self.edges_clique)
+        return
+
+    def update_plot(self, apr_eigvec2, data, save_flag, t, t_end, nodes_color, new_edges):
+        """
+        Code to generate the animation. Still a WIP, can be written better.
+        NOTE: create a folder named "animation" before running this.
+        """
+
+        plt.clf()
+
+
+        self.fig.suptitle("Time = {0:02d}".format(t), fontsize=16)
+        gs = gridspec.GridSpec(2, 2)
+
+        ax = self.fig.add_subplot(gs[0, 0])
+        ax.scatter(list(self.G.nodes), np.abs(apr_eigvec2 * np.sqrt(self.n_nodes)), s=15, color=nodes_color)
+        plt.grid(True)
+        ax.set_xlabel('Component', fontsize=12)
+
+        ax.set_xlim(-2, self.n_nodes)
+        ax.set_ylim(bottom=0)
+        ax.set_title('Eigenvector of 2nd largest eigenvalue')
+        ax.axes.get_yaxis().set_ticks([])
+
+        ax = self.fig.add_subplot(gs[0, 1])
+        nx.draw_networkx_nodes(self.G, ax=ax, node_color=nodes_color, pos=self.pos_dict, node_size=15)
+        nx.draw_networkx_edges(self.G, ax=ax, pos=self.pos_dict, width=0.1, edge_color='k')
+        nx.draw_networkx_edges(self.G, ax=ax, pos=self.pos_dict, edgelist=new_edges, width=0.4, edge_color='r')
+
+        ax.axis('off')
+
+        # start_time = int(20/save_flag) - 1
+        start_time = 0
+        ax = self.fig.add_subplot(gs[1, :])
+        ax.plot(data[start_time:int(t / save_flag), 0], data[start_time:int(t / save_flag), 1] * 100, label='Norm')
+        ax.plot(data[start_time:int(t / save_flag), 0], data[start_time:int(t / save_flag), 2] * 100, label='Threshold')
+        ax.plot(data[start_time:int(t / save_flag), 0], data[start_time:int(t / save_flag), 3], label='Number of edges')
+
+        ax.vlines(60, ymin=0, ymax=2000, label='Community formation', color='k')
+
+        plt.legend()
+        plt.grid(True)
+        ax.set_title('NEWMA', fontsize=20)
+        ax.set_xlabel('Time')
+        ax.set_xlim(start_time, t_end)
+
+        if self.save_path is not None:
+            plt.savefig(self.save_path + 'anim_{0:02d}.png'.format(t), format='png', bbox_inches='tight')
+        #self.fig.canvas.draw()
+
+        return
+
+
+    def __info__(self):
+        print("n_nodes = {}, p_edges = {}, clique_size = {}, noise_ratio = {}"
+              .format(self.n_nodes, self.p_edges, self.clique_size, self.noise_ratio))
+        return
+
+class NEWMA:
+    def __init__(self, n_nodes, n_components, time_window=20, l_ratio=8.5, eta=0.99, rescale_tau=1.07, power_iter=0, save_path=None):
+        self.n_nodes = n_nodes
+        self.time_window = time_window
+        self.l_ratio = l_ratio
+        self.n_components = n_components
+        self.eta = eta
+        self.rescale_tau = rescale_tau
+        self.power_iter = power_iter
+        self.save_path = save_path
+
+        self.lam = (self.l_ratio ** (1. / self.time_window) - 1) / (self.l_ratio ** ((self.time_window + 1) / self.time_window - 1))
+        self.LAMBDA = self.l_ratio * self.lam
+
+        self.threshold = 1
+        self.norm = 0
+        self.norm_average = 0
+        self.z1, self.z2 = np.zeros((self.n_nodes, self.n_components)), np.zeros((n_nodes, self.n_components))
+        self.detection_flag = False
+        self.log = []
+
+    def detect(self, Adj_matrix, t):
+
+        self.z1 = (1 - self.lam) * self.z1 + self.lam * Adj_matrix
+        self.z2 = (1 - self.LAMBDA) * self.z1 + self.LAMBDA * Adj_matrix
+
+        self.norm = np.linalg.norm(self.z1 - self.z2)
+
+        if self.norm > self.threshold:
+            print('Flag at t =', t)
+            self.detection_flag = True
+            den_mean = 1
+        else:
+            self.detection_flag = False
+            den_mean = t
+
+        self.update_log(n_edges=np.sum(Adj_matrix)/2, t=t)
+
+        self.threshold = self.rescale_tau * ((1 - self.eta) * self.norm_average + self.eta * self.norm)
+
+        if den_mean == 1:
+            self.norm_average = self.norm
+        else:
+            self.norm_average = (self.norm + den_mean * self.norm_average) / (den_mean + 1)
+
+        return
+
+    def compute_eigenvector(self, Adj_matrix):
+
+        #Normalization
+
+        d = np.sum(Adj_matrix, axis=1)
+        D = np.diag(d)
+        norm_A_clique = np.matmul(D, np.matmul(Adj_matrix, D))
+
+        I_A = np.ones((self.n_nodes, self.n_nodes)) + norm_A_clique
+        v1 = np.ones(self.n_nodes) * 1.0 / np.sqrt(self.n_nodes)
+
+        #Power method
+
+        x = np.random.randint(-1, 1, size=self.n_nodes)
+        eigenvec = x - np.dot(v1, x) * v1
+        for i in range(self.power_iter):
+            eigenvec = np.dot(I_A, eigenvec)
+
+        return eigenvec
+
+    def update_log(self, n_edges, t):
+
+        data_columns = ["t", "edges", "norm", "norm_average", "thresold", "detection_flag"]
+        data = [t, n_edges, self.norm, self.norm_average, self.threshold, self.detection_flag]
+        self.log.append(data)
+
+        if self.save_path is not None:
+            df = pd.DataFrame(self.log, columns=data_columns)
+            df.to_csv(os.path.join(self.save_path, "newma_{}.csv".format(self.n_nodes)), sep='\t', index=False)
