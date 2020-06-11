6a6dd110330a7f6d7e6e7409f01e5b4f28cd4978
LucaT1995 <lu.tom1995@gmail.com>
2020-06-11 15:27:29 +0200

    Remove cupy from required install

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=["numpy==1.17.5", "scikit-learn==0.22.1", "scipy==1.4.1", "torch==1.2", "torchvision==0.4",
                       "pandas==0.24.2", "tqdm==4.41.1", "matplotlib==3.0.2", "Pillow==6.2", "lightonml==1.0.2",
-                      "lightonopu==1.0.4", "networkx==2.4", "cupy==7.2.0"],
+                      "lightonopu==1.1.0", "networkx==2.4"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
